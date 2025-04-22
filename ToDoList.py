bbfb63a1917abf581da7161baa369c04f651c640
Habtamu Teklehimanot Gebregziabher <haftayteklehimanot@gmail.com>
2025-04-22 12:46:35 +0300

    Initialized the app

---
+++ ToDoList.py
@@ -0,0 +1,12 @@
+
+task=[]
+
+if __name__='__main__':
+
+    print('Welcome to your daily to list')
+    print('\n
+    1. Add Task '\n'
+    2. Delete Task'\n'
+    3. List Task'\n'
+    4. Quit task'\n')
+    while True:
