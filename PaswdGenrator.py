054bf8a80312ec5e1d7d22a97fcd9367d6369bf7
Habtamu Teklehimanot Gebregziabher <haftayteklehimanot@gmail.com>
2025-04-29 11:37:16 +0300

    I just added gemini's code

--- PaswdGenrator.py
+++ PaswdGenrator.py
@@ -1,21 +1,22 @@
+# import random as rand
+# import string as str
+# passwd_lngth=int(input('input the length of password you want to generate'))
+# characters=str.ascii_letters + str.digits + str.punctuation
+# password=''
+# for x in range(passwd_lngth):
+#     password+=rand.choice(characters)    
+# print(password) 
 import random as rand
 import string as str
 
-passwd_lngth=int(input('input the length of password you want to generate'))
-found=True
+passwd_lngth = int(input('Input the length of password you want to generate: '))
 
-# while True:
-for x in range(passwd_lngth):
-    characters=str.ascii_letters + str.digits + str.punctuation
-    paswrd=[]
+characters = str.ascii_letters + str.digits + str.punctuation
+password = ""  # Initialize an empty string to store the password
 
-    if True:
-        password=rand.choice(characters)
-        for x in password:
-            paswrd.append(password)
-            print(paswrd)
-        # return(paswrd)
-    # return
-    # break
+for _ in range(passwd_lngth):
+    password += rand.choice(characters)  # Append a random character to the password string
+
+print(password)  # Print the complete password after the loop finishes
 
 
