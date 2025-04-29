c6535f2c6e22e579ca3fd31707816c7c87db8f19
Habtamu Teklehimanot Gebregziabher <haftayteklehimanot@gmail.com>
2025-04-29 12:33:00 +0300

    FInished

--- PaswdGenrator.py
+++ PaswdGenrator.py
@@ -1,22 +1,8 @@
-# import random as rand
-# import string as str
-# passwd_lngth=int(input('input the length of password you want to generate'))
-# characters=str.ascii_letters + str.digits + str.punctuation
-# password=''
-# for x in range(passwd_lngth):
-#     password+=rand.choice(characters)    
-# print(password) 
 import random as rand
 import string as str
-
-passwd_lngth = int(input('Input the length of password you want to generate: '))
-
-characters = str.ascii_letters + str.digits + str.punctuation
-password = ""  # Initialize an empty string to store the password
-
-for _ in range(passwd_lngth):
-    password += rand.choice(characters)  # Append a random character to the password string
-
-print(password)  # Print the complete password after the loop finishes
-
-
+passwd_lngth=int(input('input the length of password you want to generate'))
+characters=str.ascii_letters + str.digits + str.punctuation
+password=''
+for x in range(passwd_lngth):
+    password+=rand.choice(characters) 
+print(list(password)) 
